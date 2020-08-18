a3a0e72c294e04e3dd96f49aa1cf8d1e93ba2e91
macwebdevelop <36200354+macwebdevelop@users.noreply.github.com>
2020-08-17 23:02:27 -0400

    Alpha version of Vanilla JS Expense Tracker App
    Expense/Income/Transaction tracking app with sqlite3 database.

---
+++ Expense Tracker Vanilla/app.js
@@ -0,0 +1,166 @@
+var process = require('process');
+
+var port = parseInt(process.argv[2]);
+var portal = port + 1;
+
+var express = require('express');
+const fs = require('fs');
+const WebSocketServer = require('ws');
+const wss = new WebSocketServer.Server({port: portal});
+
+var app = express();
+
+const sqlite3 = require('sqlite3').verbose();
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+let db = new sqlite3.Database('db/database.db', function (err) {
+	if (err) {
+		console.error(err.message);
+	}
+	console.log('Connected to the database.');
+});
+
+db.serialize(function () {
+  db.run("CREATE TABLE IF NOT EXISTS balance (id INTEGER PRIMARY KEY AUTOINCREMENT, amount DECIMAL(12, 2) NOT NULL, text VARCHAR(100) NOT NULL);");
+});
+
+
+app.use(express.static('static_files'));
+
+app.get('/api/getTransactions/', function(req, res){
+	var transactions = [];
+  console.log("GET: Transactions");
+
+  let sql = 'SELECT * FROM balance;';
+  db.all(sql, [], function(err, rows){
+		var result = {};
+    if(err){
+      res.status(404);
+      result["error"] = err.message;
+			res.json(result);
+			console.log(JSON.stringify(result));
+    } else {
+      rows.forEach((row) => {
+				transactions.push(row);
+      });
+			res.json(transactions);
+      console.log(JSON.stringify(transactions));
+    }
+  });
+});
+
+app.get('/api/getBalance/', function(req, res){
+  console.log("GET: Balance");
+
+  let sql = 'SELECT ROUND(TOTAL(amount), 2) FROM balance;';
+  db.get(sql, [], function(err, row){
+    var result = {};
+    if(err){
+      res.status(400);
+      result["error"] = err.message;
+    } else {
+      result["balance"] = row;
+    }
+    console.log(JSON.stringify(result));
+    res.json(result);
+  });
+});
+
+app.get('/api/getIncome/', function(req, res){
+  console.log("GET: Income");
+
+  let sql = 'SELECT ROUND(TOTAL(amount), 2) FROM balance WHERE amount > 0;';
+  db.get(sql, [], function(err, row){
+    var result = {};
+    if(err){
+      res.status(400);
+      result["error"] = err.message;
+    } else {
+      result["income"] = row;
+    }
+    console.log(JSON.stringify(result));
+    res.json(result);
+  });
+});
+
+app.get('/api/getExpenses/', function(req, res){
+  console.log("GET: Expenses");
+
+  let sql = 'SELECT ROUND(TOTAL(amount), 2) FROM balance WHERE amount < 0;';
+  db.get(sql, [], function(err, row){
+    var result = {};
+    if(err){
+      res.status(400);
+      result["error"] = err.message;
+    } else {
+      result["expense"] = row;
+    }
+    console.log(JSON.stringify(result));
+    res.json(result);
+  });
+});
+
+app.post('/api/addTransaction/', function(req, res){
+  var text = req.body.text;
+  var amount = req.body.amount;
+  console.log("POST: "+text+" - "+amount);
+
+  let sql = 'INSERT INTO balance (text, amount) VALUES (?, ?);';
+  db.run(sql, [text, amount], function(err){
+    var result = {};
+    if(err){
+      res.status(400);
+      result["error"] = err.message;
+    } else {
+      result["response"] = "Insert Successful: "+text;
+    }
+    console.log(JSON.stringify(result));
+    res.json(result);
+  });
+});
+
+app.delete('/api/deleteTransaction/', function(req, res){
+  var id = req.body.id;
+  console.log("DELETE: Transaction ID - "+id);
+
+  let sql = 'DELETE FROM balance WHERE id = ?;';
+  db.run(sql, [id], function(err){
+    var result = {};
+    if(err){
+      res.status(409);
+      result["error"] = err.message;
+    } else {
+      result["transactionDeleted"] = "Deleted Transaction: "+id;
+    }
+    console.log(JSON.stringify(result));
+    res.json(result);
+  });
+});
+
+app.delete('/api/deleteAll/', function(req, res){
+	console.log("DELETE: All Transactions");
+
+	let sql = 'DELETE FROM balance;';
+	db.run(sql, [], function(err){
+		var result = {};
+		if(err){
+			res.status(400);
+			result["error"] = err.message;
+		} else {
+			result["deleteAll"] = "Deleted all transactions";
+		}
+		console.log(JSON.stringify(result));
+		res.json(result);
+	});
+});
+
+app.use('/', function(req,rest,next){
+	next();
+});
+
+app.listen(port, function () {
+  	console.log('Example app listening on port '+port);
+});
---
+++ Expense Tracker Vanilla/static_files/transactionList.js
@@ -0,0 +1,131 @@
+function createTL() {
+  $.ajax({
+    method: "GET",
+    url: "/api/getTransactions/",
+    data: {}
+  }).done(function(rows){
+    console.log("Request to fetch transactions");
+    if("error" in rows){
+      console.log(rows["error"]);
+    } else {
+      $("#tl").html('')
+      var tList = '';
+      for(var i=0; i<rows.length; i++){
+        tList += '<li>ID: ' + JSON.stringify(rows[i]["id"]) + ' - ' + JSON.stringify(rows[i]["text"]) + '- $' + JSON.stringify(rows[i]["amount"]) + '</li>';
+      }
+      $("#tl").html(tList);
+    }
+  });
+}
+
+function getBalance(){
+  $.ajax({
+    method: "GET",
+    url: "/api/getBalance/",
+    data: {}
+  }).done(function(data){
+    if("error" in data){
+      console.log(data["error"]);
+    } else {
+      console.log(JSON.stringify(data["balance"]["ROUND(TOTAL(amount), 2)"]));
+      document.getElementById('balance').innerHTML = JSON.stringify(data["balance"]["ROUND(TOTAL(amount), 2)"]);
+    }
+  });
+}
+
+function getIncome(){
+  $.ajax({
+    method: "GET",
+    url: "/api/getIncome/",
+    data: {}
+  }).done(function(data){
+    if("error" in data){
+      console.log(data["error"]);
+    } else {
+      console.log(JSON.stringify(data["income"]["ROUND(TOTAL(amount), 2)"]));
+      document.getElementById('income').innerHTML = JSON.stringify(data["income"]["ROUND(TOTAL(amount), 2)"]);
+    }
+  });
+}
+
+function getExpenses(){
+  $.ajax({
+    method: "GET",
+    url: "/api/getExpenses/",
+    data: {}
+  }).done(function(data){
+    if("error" in data){
+      console.log(data["error"]);
+    } else {
+      console.log(JSON.stringify(data["expense"]["ROUND(TOTAL(amount), 2)"]));
+      document.getElementById('expense').innerHTML = JSON.stringify(data["expense"]["ROUND(TOTAL(amount), 2)"]);
+    }
+  });
+}
+
+function addTransaction(){
+  $.ajax({
+    method: "POST",
+    url: "/api/addTransaction/",
+    data: {
+      text: $("#label").val(),
+      amount: $("#amount").val()
+    }
+  }).done(function(data){
+    console.log("Request to add transaction: " + JSON.stringify(data));
+    if("error" in data){
+      console.log(data["error"]);
+    } else{
+        console.log(data["transaction"])
+        createTL();
+        getBalance();
+        getIncome();
+        getExpenses();
+    }
+  });
+}
+
+function deleteTransaction(id) {
+  $.ajax({
+    method: "DELETE",
+    url: "/api/deleteTransaction/",
+    data: {
+      id: $("#identifier").val()
+    }
+  }).done(function(data){
+    console.log("Request to delete transaction with ID: "+ id);
+    if("error" in data){
+      console.log(data["error"]);
+    } else {
+      console.log(data["transactionDeleted"]);
+      createTL();
+      getBalance();
+      getIncome();
+      getExpenses();
+    }
+  });
+}
+
+function deleteAll(){
+  $.ajax({
+    method: "DELETE",
+    url: "/api/deleteAll/",
+    data: {}
+  }).done(function(data){
+    console.log("Request to delete all transactions");
+    if("error" in data) {
+      console.log(data["error"]);
+    } else {
+      console.log(data["deleteAll"]);
+      createTL();
+      getBalance();
+      getIncome();
+      getExpenses();
+    }
+  });
+}
+
+$(function(){
+  $("#addSubmit").on('click', function(){ addTransaction(); });
+  $("#deleteSubmit").on('click', function(){ deleteTransaction(); });
+});
